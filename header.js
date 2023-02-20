eec838db59ea54c8977d112494948b06519dcdd9
abdelhaqelamraoui <abdelhaqelamraoui@gmail.com>
2023-02-20 22:36:19 +0100

    Fixing bugs + archiving Feat. + improving UI ..

---
+++ header.js
@@ -0,0 +1,6 @@
+
+let aboutLink = document.getElementById('link-about');
+
+aboutLink.addEventListener('click', () => {
+  alert('App devolopped by: \n\tAbdelhaq EL AMRAOUI\n\t2023');
+});
---
+++ index.js
@@ -0,0 +1,5 @@
+
+
+let listUploadedFiles = document.getElementById('list-uploaded-files');
+
+listUploadedFiles.innerHTML = '';
